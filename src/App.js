d032557c2d37e01c9495231eeba64f9d76f62921
garofalof <fgarofalo1493@gmail.com>
2022-04-13 15:19:11 -0700

    completed sorting mini app

---
+++ src/App.js
@@ -0,0 +1,209 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+
+const App = () => {
+  const [nums, setNums] = useState([]);
+  const [active, setActive] = useState([]);
+  const [display, setDisplay] = useState([]);
+  const [delay, setDelay] = useState(10);
+  const [slider, setSlider] = useState(10);
+
+  const timer = (delay) => {
+    return new Promise((resolve) => setTimeout(resolve, delay));
+  }
+
+  const initialize = async () => {
+    setNums([]);
+    for (let i = 1; i <= 75; i++) {
+      setNums((prev) => prev.concat(i));
+      await timer(delay);
+    }
+  };
+
+  const shuffle = async() => {
+    for (let i = 0; i < nums.length; i++) {
+      let rand = Math.floor(Math.random() * nums.length);
+      setActive([i, rand]);
+      setNums((arr) => {
+        let temp  = [...arr];
+        [temp[i], temp[rand]] = [temp[rand], temp[i]];
+        return temp;
+      });
+      await timer(delay);
+    }
+    setActive([]);
+  }
+
+  const insertionSort = async() => {
+    let numsArr = [...nums];
+    for (let i = 1; i < numsArr.length; i++) {
+      let j = i - 1;
+      let temp = numsArr[i];
+      while (j >= 0 && numsArr[j] > temp) {
+        setActive([j, j + 1]);
+        setNums((arr) => {
+          let prev = [...arr];
+          prev[j + 1] = prev[j];
+          return prev;
+        });
+        await timer(delay);
+        numsArr[j + 1] = numsArr[j];
+        j--;
+      }
+      setActive([j + 1, i]);
+      setNums((arr) => {
+        let prev = [...arr];
+        prev[j + 1] = temp;
+        return prev;
+      });
+      await timer(delay);
+      numsArr[j + 1] = temp;
+    }
+    setActive([]);
+  }
+
+  const bubbleSort = async() => {
+    let numsArr = [...nums];
+    let len = numsArr.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len; j++) {
+        if (numsArr[j] > numsArr[j + 1]) {
+          setActive([j, j + 1]);
+          setNums((arr) => {
+            let prev = [...arr];
+            [prev[j], prev[j + 1]] = [prev[j + 1], prev[j]];
+            return prev;
+          });
+          await timer(delay);
+          [numsArr[j], numsArr[j + 1]] = [numsArr[j + 1], numsArr[j]];
+        }
+      }
+    }
+    setActive([]);
+  }
+
+  const selectionSort = async() => {
+    let min;
+    let numsArr = [...nums];
+    for (let i = 0; i < numsArr.length; i++) {
+      min = i;
+      setActive([min]);
+      await timer(delay);
+      for (let j = i + 1; j < numsArr.length; j++) {
+        if (numsArr[j] < numsArr[min]) {
+          min = j;
+          setActive([min]);
+          await timer(delay);
+        }
+      }
+      if (min !== i) {
+        setActive([i, min]);
+        setNums((arr) => {
+          let prev = [...arr];
+          [prev[i], prev[min]] = [prev[min], prev[i]];
+          return prev;
+        });
+        await timer(delay);
+        [numsArr[i], numsArr[min]] = [numsArr[min], numsArr[i]];
+      }
+    }
+    setActive([]);
+  }
+
+  const quickSort = () => {
+    async function partition(arr, start, end) {
+      let pivotVal = arr[end];
+      let pivotIndex = start;
+      for (let i = start; i < end; i++) {
+        if (arr[i] < pivotVal) {
+          setActive([i, pivotIndex]);
+          setNums((arr) => {
+            let prev = [...arr];
+            [prev[i], prev[pivotIndex]] = [prev[pivotIndex], prev[i]];
+            return prev;
+          });
+          await timer(delay);
+          [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+          pivotIndex++;
+        }
+      }
+      setActive([pivotIndex, end]);
+      setNums((arr) => {
+        let prev = [...arr];
+        [prev[pivotIndex], prev[end]] = [prev[end], prev[pivotIndex]];
+        return prev;
+      });
+      await timer(delay);
+      [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+      return pivotIndex;
+    }
+
+    async function sort(arr) {
+      let stack = [];
+      stack.push(0);
+      stack.push(arr.length - 1);
+
+      while (stack[stack.length - 1] >= 0) {
+        let end = stack.pop();
+        let start = stack.pop();
+        let pivotIndex = await partition(arr, start, end);
+        setActive([]);
+        await timer(delay);
+        if (pivotIndex - 1 > start) {
+          stack.push(start);
+          stack.push(pivotIndex - 1);
+        }
+        if (pivotIndex + 1 < end) {
+          stack.push(pivotIndex + 1);
+          stack.push(end);
+        }
+      }
+    }
+    let temp = [...nums];
+    sort(temp);
+  }
+
+  const handleChange = (e) => {
+    setSlider(e.target.value);
+    setDelay(e.target.value);
+  }
+
+  useEffect(() => {
+    let render = nums.map((num, index) => {
+      if (index === active[0] || index === active[1]) {
+        return <div className='bar-active' style={{height: `${num / 1}%`, width: `${80 / nums.length}%`}}></div>;
+      } else {
+        return <div className='bar' style={{height: `${num / 1}%`, width: `${80 / nums.length}%`}}></div>;
+      }
+    });
+    setDisplay(render);
+  }, [nums, active, slider]);
+
+  return (
+    <div style={{height: '770px'}} className="app">
+      <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'flex-start', paddingTop: '20px', backgroundImage: 'linear-gradient(180deg, black, #1C1352, rgb(29, 60, 122))', height: '125px'}}>
+        <div style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-end', paddingTop: '5px'}}>
+          <button className="button" onClick={initialize}>Initialize</button>
+          <button className="button" onClick={shuffle}>Shuffle</button>
+        </div>
+        <div className="slide-container">
+          <span style={{paddingBottom: '10px', fontWeight: '400'}}>Animation Speed</span>
+          <input onChange={handleChange} type="range" min="1" max="250" value={slider} className="slider" id="myRange"></input>
+          <span style={{paddingTop: '10px'}}>{slider + 'ms'}</span>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '15px'}}>
+          <button className="button-sort" onClick={insertionSort}>Insertion Sort</button>
+          <button className="button-sort" onClick={selectionSort}>Selection Sort</button>
+          <button className="button-sort" onClick={quickSort}>Quick Sort</button>
+          <button className="button-sort" onClick={bubbleSort}>Bubble Sort</button>
+        </div>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '0px', paddingRight: '50px', paddingLeft: '50px', height: '80%'}}>
+        { display }
+      </div>
+      <div style={{width: '100%', height: '100%', backgroundImage: 'linear-gradient(180deg, #1C1352, black', maxHeight: '185px'}}></div>
+    </div>
+  );
+}
+
+export default App;
